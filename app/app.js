b631d1601e590b5d095f6c045790026c03c8d538
Abhinavvisen <visenabhinav96@gmail.com>
2016-12-03 15:33:21 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var myNinjaApp = angular.module('myNinjaApp', ['ngRoute','ngAnimate]);
+var myNinjaApp = angular.module('myNinjaApp', ['ngRoute','ngAnimate']);
 
 myNinjaApp.config(['$routeProvider', function($routeProvider){
 
